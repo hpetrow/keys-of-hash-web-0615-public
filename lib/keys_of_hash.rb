73ee4276e68b2d79fa9e5328b9ef867f80c679d2
hpetrow <heather.petrow@gmail.com>
2015-06-09 11:08:33 -0400

    done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,8 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    self.collect{ |animal, country|
+      animal if arguments.include?(country)
+    }.compact
   end
 end
